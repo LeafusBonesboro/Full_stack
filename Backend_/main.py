f5fe992805d53ed360b291a25a549161d7a26791
LeafusBonesboro <108142251+LeafusBonesboro@users.noreply.github.com>
2024-05-16 18:32:34 -0400

    Basic code
    coded routes, changed environment, put in contact api

--- Backend_/main.py
+++ Backend_/main.py
@@ -1,1 +1,39 @@
 #main routes or main endpoints. for bigger apps can seperate into multiple files
+from config import app, db
+from flask import jsonify, request
+from models import Contact
+
+
+@app.route("/contacts", methods=["GET"])
+def get_contacts():
+    contacts = Contact.query.all()
+    json_contacts = list(map(lambda x: x.to_json(), contacts))
+    return jsonify({"contacts": json_contacts})
+
+@app.routh("/create_contact", methods=["POST"])
+def create_contact():
+    first_name = request.json.get("firstName")
+    last_name = request.jason.get("lastName")
+    email = request.json.get("email")
+
+    if not first_name or not last_name or not email:
+        return (
+            jsonify({"message": "Your must include a first name, last name and email"}),
+            400,
+        )
+
+    new_contact = Contact(first_name=first_name, last_name=last_name, email=email)
+    try:
+        db.session.add(new_contact)
+        db.session.commit()
+    except Exception as e:
+        return jsonify({"message": str(e)}), 400
+    
+    return jsonify({"message": "User created!"}), 201
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+
+    app.run(debug=True)
